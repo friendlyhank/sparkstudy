aa5be915e6f6be5bd9a29619ab097730ba7222da
hank <hanguang8888@outlook.com>
2019-05-17 23:40:40 +0800

    JavaWordCount

--- src/main/java/com/hank/example/JavaWordCount.java
+++ src/main/java/com/hank/example/JavaWordCount.java
@@ -1,7 +1,9 @@
 package com.hank.example;
 
+import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.sql.SparkSession;
 import scala.Tuple2;
 
@@ -18,14 +20,29 @@ public class JavaWordCount {
             System.exit(1);
         }
 
-        //SparkSession
-        SparkSession spark = SparkSession
-                .builder()
-                .appName("JavaWordCount")
-                .getOrCreate();
+        //(1)
+        //创建Sparkconf对象,设置Spark应用的配置信息
+        //SetMaster可以设置Spark应用程序要连接spark集群的master节点的url;但是如果为local则为本地
+        SparkConf conf = new SparkConf()
+                .setAppName("WordCountLocal")
+                .setMaster("local");
 
-        //readfile 读取文件
-        JavaRDD<String> lines= spark.read().textFile(args[0]).javaRDD();
+        //创建JavaSparkContext对象
+        //在spark中,JavaSparkContext是Spark所有功能的入口
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+
+        JavaRDD<String> lines = sc.textFile(args[0]);
+
+        //(2)
+        //Spark-SQL包 SparkSession
+//        SparkSession spark = SparkSession
+//                .builder()
+//                .appName("JavaWordCount")
+//                .getOrCreate();
+//
+//        //readfile 读取文件
+//        JavaRDD<String> lines= spark.read().textFile(args[0]).javaRDD();
 
         //faltmap 每个元素进行分割符分割
         JavaRDD<String> words = lines.flatMap(s -> Arrays.asList(SPACE.split(s)).iterator());

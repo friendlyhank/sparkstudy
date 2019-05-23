528682044bd34517439188382aec9e773798a98f
hank <hanguang8888@outlook.com>
2019-05-23 23:32:14 +0800

    filter

--- src/main/java/com/hank/example/TransformationOperation.java
+++ src/main/java/com/hank/example/TransformationOperation.java
@@ -14,11 +14,12 @@ import java.util.List;
  */
 public class TransformationOperation {
     public static void main(String[] args) {
-        map();
+//        map();
+        filter();
     }
 
     /**
-     * map转化:
+     * map转化:每个元素乘以2
      */
     private static void map(){
          SparkConf conf = new SparkConf().
@@ -41,6 +42,34 @@ public class TransformationOperation {
             @Override
             public void call(Integer t) throws Exception {
                 System.out.println(t);
+            }
+        });
+    }
+
+    /**
+     * filter转化:过滤集合中的偶数
+     */
+    private static void filter(){
+        SparkConf conf = new SparkConf().
+                setAppName("filter").setMaster("local");
+
+        //JavaSparkContext
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8,9);
+        JavaRDD<Integer> numberRdd = sc.parallelize(numbers);
+
+        JavaRDD<Integer> answer = numberRdd.filter(new Function<Integer, Boolean>() {
+            @Override
+            public Boolean call(Integer integer) throws Exception {
+                return integer%2 != 0;
+            }
+        });
+
+        answer.foreach(new VoidFunction<Integer>() {
+            @Override
+            public void call(Integer integer) throws Exception {
+                System.out.println(integer);
             }
         });
     }

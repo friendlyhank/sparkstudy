c62ad4e510c7b6943790df22783f88a0acfd239d
hank <hanguang8888@outlook.com>
2019-05-27 00:06:19 +0800

    persist

--- src/main/java/com/hank/example/Persist.java
+++ src/main/java/com/hank/example/Persist.java
@@ -19,6 +19,7 @@ public class Persist {
 
         JavaSparkContext sc = new JavaSparkContext(conf);
 
+        //必须在创建RDD后加cache,换行lines.cache()无效
         JavaRDD<String> lines = sc.textFile("D://ideaproject//sparkstudy//files//spark.txt")
                 .cache();
 
@@ -38,6 +39,8 @@ public class Persist {
 
         endTime = System.currentTimeMillis();
         System.out.println("cost " + (endTime - beginTime) + " milliseconds.");
+
+        //第一次count后，第二次count不cache()的情况，也会快很多，只是cache能更快
 
         sc.close();
     }

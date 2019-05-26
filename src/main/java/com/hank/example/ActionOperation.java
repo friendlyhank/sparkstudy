f2074853f46b66dce99058f191d370ba4c313547
hank <hanguang8888@outlook.com>
2019-05-26 22:30:41 +0800

    countByKey

--- src/main/java/com/hank/example/ActionOperation.java
+++ src/main/java/com/hank/example/ActionOperation.java
@@ -1,13 +1,16 @@
 package com.hank.example;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
+import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public class ActionOperation {
     public static void main(String[] args) {
@@ -15,7 +18,8 @@ public class ActionOperation {
 //        collect();
 //        count();
 //        take();
-
+//        saveAsTextFile();
+        countByKey();
     }
 
     /**
@@ -44,6 +48,9 @@ public class ActionOperation {
         sc.close();
     }
 
+    /**
+     * collect行动操作: 不要再集群中使用，消耗大量资源把元素集合到一台机子上
+     */
     private static void collect(){
         SparkConf conf = new SparkConf().setMaster("local").setAppName("collect");
 
@@ -69,6 +76,9 @@ public class ActionOperation {
        sc.close();
     }
 
+    /**
+     * count行动操作， 元素的个数
+     */
     private static void count(){
         SparkConf conf = new SparkConf().setMaster("local").setAppName("count");
 
@@ -85,6 +95,9 @@ public class ActionOperation {
         sc.close();
     }
 
+    /**
+     * take行动操作 获取前4个元素
+     */
     private static void take(){
         SparkConf conf = new SparkConf().setMaster("local").setAppName("take");
 
@@ -98,6 +111,56 @@ public class ActionOperation {
 
         for(Integer num : tokenNums){
             System.out.println(num);
+        }
+    }
+
+    /**
+     * saveAsTextFile行动操作  将结果保存在hdfs(这个要掌握)
+     * 在window会报错，因为不是root用户没有权限操作
+     */
+    private static void saveAsTextFile(){
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("saveAsTextFile");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> numberList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        JavaRDD<Integer> numbers = sc.parallelize(numberList);
+
+        JavaRDD<Integer> doubleNumbers = numbers.map(new Function<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) throws Exception {
+                return integer *2 ;
+            }
+        });
+
+        doubleNumbers.saveAsTextFile("hdfs://spark1:8020/spark/double_number.txt");
+
+        sc.close();
+    }
+
+    /**
+     * 键值对行动操作：每个键对应的元素计数
+     */
+    private static void countByKey(){
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("countByKey");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Tuple2<String,String>> scoreList =Arrays.asList(
+                new Tuple2<String,String>("class1","leo"),
+                new Tuple2<String,String>("class2","jack"),
+                new Tuple2<String,String>("class1","marry"),
+                new Tuple2<String,String>("class2","tom"),
+                new Tuple2<String,String>("class2","david")
+        );
+
+        JavaPairRDD<String,String> students= sc.parallelizePairs(scoreList);
+
+        Map<String,Long> studentCounts = students.countByKey();
+
+        for(Map.Entry<String,Long> studentCount : studentCounts.entrySet()){
+            System.out.println(studentCount.getKey()+"："+studentCount.getValue());
         }
     }
 }

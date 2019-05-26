00b274d3e58de3bc48336ae430cf0484c7d05f25
hank <hanguang8888@outlook.com>
2019-05-26 22:00:48 +0800

    token

--- src/main/java/com/hank/example/ActionOperation.java
+++ src/main/java/com/hank/example/ActionOperation.java
@@ -3,6 +3,7 @@ package com.hank.example;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 
 import java.util.Arrays;
@@ -10,9 +11,17 @@ import java.util.List;
 
 public class ActionOperation {
     public static void main(String[] args) {
-        reduce();
+//        reduce();
+//        collect();
+//        count();
+//        take();
+
     }
 
+    /**
+     * map是转换操作，对每个元素进行操作返回新的元素
+     * reduce是行动操作，讲元素聚合求值
+     */
     private static void reduce(){
         SparkConf conf = new SparkConf().setMaster("local").setAppName("reduce");
 
@@ -33,5 +42,62 @@ public class ActionOperation {
 
         //关闭JavaSparkContext
         sc.close();
+    }
+
+    private static void collect(){
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("collect");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> numberList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        JavaRDD<Integer> numbers = sc.parallelize(numberList);
+
+        JavaRDD<Integer> doubleNumbers = numbers.map(new Function<Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer) throws Exception {
+                return integer*2;
+            }
+        });
+        
+        List<Integer> doubleNumberList = doubleNumbers.collect();
+
+       for(Integer num : doubleNumberList){
+           System.out.println(num);
+       }
+
+       sc.close();
+    }
+
+    private static void count(){
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("count");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> numberList = Arrays.asList(1,2,3,4,6,7,8,9,10);
+
+        JavaRDD<Integer> numbers = sc.parallelize(numberList);
+
+        long sums = numbers.count();
+
+        System.out.println(sums);
+
+        sc.close();
+    }
+
+    private static void take(){
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("take");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        List<Integer> numberList = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        JavaRDD<Integer> numbers = sc.parallelize(numberList);
+
+        List<Integer> tokenNums = numbers.take(4);
+
+        for(Integer num : tokenNums){
+            System.out.println(num);
+        }
     }
 }

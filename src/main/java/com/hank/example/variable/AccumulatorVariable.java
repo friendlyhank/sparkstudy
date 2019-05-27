7a98d16de1e457993ad76e5fc5fbcbe63d636785
hank <hanguang8888@outlook.com>
2019-05-27 23:10:08 +0800

    broadcast

---
+++ src/main/java/com/hank/example/variable/AccumulatorVariable.java
@@ -0,0 +1,7 @@
+package com.hank.example.variable;
+
+/**
+ * 共享变量高级特性：Broadcast广播变量(重点掌握)
+ */
+public class AccumulatorVariable {
+}
---
+++ src/main/java/com/hank/example/variable/BroadcastVariable.java
@@ -0,0 +1,44 @@
+package com.hank.example.variable;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.Function;
+import org.apache.spark.api.java.function.VoidFunction;
+import org.apache.spark.broadcast.Broadcast;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 共享变量高级特性：Broadcast广播变量(重点掌握)
+ */
+public class BroadcastVariable {
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local").setAppName("BroadcastVariable");
+
+        JavaSparkContext sc =new JavaSparkContext(conf);
+
+        final int factor =3;
+        Broadcast<Integer> factorBroadcast= sc.broadcast(factor);
+
+        List<Integer> numberlist = Arrays.asList(1,2,3,4,5);
+
+        JavaRDD<Integer> numbers = sc.parallelize(numberlist);
+
+        JavaRDD<Integer> multipleNumbers = numbers.map(new Function<Integer, Integer>() {
+            @Override
+            public Integer call(Integer v1) throws Exception {
+                int factor = factorBroadcast.value();
+                return v1 * factor;
+            }
+        });
+
+        multipleNumbers.foreach(new VoidFunction<Integer>() {
+            @Override
+            public void call(Integer t) throws Exception {
+                System.out.println(t);
+            }
+        });
+    }
+}
